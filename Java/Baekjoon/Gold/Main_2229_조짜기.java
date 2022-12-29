3d3f20d02c3d2cce47c294072571d023456d89e2
Yungi <luk951202@naver.com>
2022-12-29 19:19:52 +0900

    BOJ 2229 조짜기

---
+++ Java/Baekjoon/Gold/Main_2229_조짜기.java
@@ -0,0 +1,37 @@
+package Java.Baekjoon.Gold;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_2229_조짜기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+
+        int N, score[], dp[];
+        N = Integer.parseInt(br.readLine());
+        score = new int[N + 1];
+        dp = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            score[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[2] = Math.abs(score[1] - score[2]);
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1];
+            for (int j = 2; j <= i; j++) {
+                dp[i] = Integer.max(dp[i], dp[i - j] + Math.abs(score[i] - score[i - j + 1]));
+            }
+        }
+
+        bw.write(String.valueOf(dp[N]));
+        bw.flush();
+        bw.close();
+    }
+}
