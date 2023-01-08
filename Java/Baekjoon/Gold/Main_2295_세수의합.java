77a145ea4a71d3e1892be8f8d0b5c2a7c95e517a
Yungi <luk951202@naver.com>
2023-01-08 21:56:14 +0900

    Refactor BOJ 2295 세 수의 합

--- Java/Baekjoon/Gold/Main_2295_세수의합.java
+++ Java/Baekjoon/Gold/Main_2295_세수의합.java
@@ -1,25 +1,18 @@
 package Java.Baekjoon.Gold;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.io.*;
+import java.util.*;
 
 public class Main_2295_세수의합 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N, num[];
-        Set<Integer> sum_set = new TreeSet<>();
-        ArrayList<Integer> sum_list;
+        int N, num[], sum[], count = 0;
 
         N = Integer.parseInt(br.readLine());
         num = new int[N];
+        sum = new int[N * N];
 
         for (int i = 0; i < N; i++) {
             num[i] = Integer.parseInt(br.readLine());
@@ -28,26 +21,18 @@ public class Main_2295_세수의합 {
 
         for (int i = 0; i < N; i++) {
             for (int j = i; j < N; j++) {
-                sum_set.add(num[i] + num[j]);
+                sum[count++] = num[i] + num[j];
             }
         }
+        Arrays.sort(sum, 0, count - 1);
 
-        sum_list = new ArrayList<>(sum_set);
         for (int i = N - 1; 0 <= i; i--) {
             for (int j = i; 0 <= j; j--) {
-                int n = num[i] - num[j];
-                int left = 0, right = sum_list.size() - 1;
-                while (left <= right) {
-                    int mid = sum_list.get((right + left) / 2);
-                    if (n == mid) {
-                        bw.write(String.valueOf(num[i]));
-                        bw.flush();
-                        bw.close();
-                        return;
-                    }
-                    else if (n < mid) right = (right + left) / 2 - 1;
-                    else left = (right + left) / 2 + 1;
-                }
+                if (Arrays.binarySearch(sum, 0, count - 1, num[i] - num[j]) < 0) continue;
+                bw.write(String.valueOf(num[i]));
+                bw.flush();
+                bw.close();
+                return;
             }
         }
     }
