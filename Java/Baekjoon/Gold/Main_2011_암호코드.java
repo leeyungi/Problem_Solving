29cf19e47b5d37ef148d97395e90067ea3970784
Yungi <luk951202@naver.com>
2023-01-16 20:18:54 +0900

    BOJ 2011 암호코드

--- Java/Baekjoon/Gold/Main_2011_암호코드.java
+++ Java/Baekjoon/Gold/Main_2011_암호코드.java
@@ -15,39 +15,23 @@ public class Main_2011_암호코드 {
         N = num.length();
         dp = new int[N];
 
-        if (num.charAt(0) != '0') dp[0] = 1;
-        if (dp[0] != 0 && 1 < N) {
-            if (num.charAt(0) == '1'){
-                if (num.charAt(1) == '0') dp[1] = 1;
-                else dp[1] = 2;
+        if (num.charAt(0) != '0') {
+            dp[0] = 1;
+            for (int i = 1; i < N; i++) {
+                int ten, one;
+                ten = (num.charAt(i - 1) -'0') * 10;
+                one = num.charAt(i) - '0';
+                if (1 <= one && one <= 9) {
+                    if (i == 1) dp[i]++;
+                    else dp[i] += dp[i - 1];
+                }
+                if (10 <= ten + one && ten + one <= 26) {
+                    if (i == 1) dp[i]++;
+                    else dp[i] += dp[i - 2];
+                }
+                dp[i] %= 1000000;
+                if (dp[i] == 0) break;
             }
-            else if (num.charAt(0) == '2') {
-                if (num.charAt(1) == '0' || '6' < num.charAt(1)) dp[1] = 1;
-                else dp[1] = 2;
-            }
-            else if (num.charAt(1) != '0') dp[1] = 1;
-        }
-
-        for (int i = 2; i < N; i++) {
-            int now, before = 0;
-            now = num.charAt(i) - '0';
-            before = num.charAt(i - 1) - '0';
-
-            if (now == 0) {
-                if (before != 1 && before != 2) break;
-                dp[i] = dp[i - 2];
-            }
-            else if (before == 0){
-                dp[i] = dp[i - 1];
-            }
-            else if (before == 1) {
-                dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000;
-            }
-            else if (before == 2) {
-                if (6 < now) dp[i] = dp[i - 1];
-                else dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000;
-            }
-            else dp[i] = dp[i - 1];
         }
 
         bw.write(String.valueOf(dp[N - 1]));
